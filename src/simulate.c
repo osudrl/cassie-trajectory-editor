2b5b2799ced9426d16be53d029ea30fd8b5e104a
Kevin Kellar <kellar.kevin@gmail.com>
2018-08-24 14:47:26 -0700

    slowed down node height 10x

--- src/simulate.c
+++ src/simulate.c
@@ -1001,7 +1001,7 @@ void scroll(GLFWwindow* window, double xoffset, double yoffset)
         if(!mod_shift)
             traj_info.selection.nodesigma *= mult;
         else
-            traj_info.selection.nodeheight *= 50 * mult;
+            traj_info.selection.nodeheight *= 5 * mult;
 
 
     }
